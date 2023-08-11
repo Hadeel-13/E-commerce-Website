101e76338ea5aa5f15b2fd286eb6fdf9c79cec29
Hadeel-13 <meera19y@gmail.com>
2023-08-11 11:22:50 -0700

    e-commerce

--- src/Components/User/UserFavoriteProducts.jsx
+++ src/Components/User/UserFavoriteProducts.jsx
@@ -2,7 +2,7 @@ import ProductCard from "../Product/ProductCard";
 export default function UserFavoriteProducts() {
     return (
         <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-7">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-7 place-items-center">
                 {Array.from(Array(11)).map((_, i) => (
                     <ProductCard key={i} />
                 ))}
--- src/Sections/Home/ProductsHome.jsx
+++ src/Sections/Home/ProductsHome.jsx
@@ -5,7 +5,7 @@ export default function ProductsHome({ title, btnTitle, path }) {
     return (
         <>
             <SubTitle title={title} btnTitle={btnTitle} path={path} className={"mt-12 mb-5"} />
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-7">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-7 place-items-center">
                 {Array.from(Array(6))
                     .slice(0, 4)
                     ?.map((cat, index) => (
